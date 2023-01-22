1d28e66606494d6b70cf30ada86f75c177fd4329
samahrens <ahrens5@purdue.edu>
2023-01-21 22:54:44 -0500

    added the footer

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,24 @@
+import companyLogo from './multiplebananas.svg'
+import arrow from './whitedownarrwo.png';
+
 export function Index() {
+
   return <div>
-    Index
+
+<body>
+
+<div class="parallax">
+  <img class="mainLogo" src={companyLogo} alt="Banana Logo" />
+  <p class="name123">Padding Split</p>
+</div>
+
+<a class="theDown" href="#down"><div class="downArrow bounce"><img width="40" height="40" alt="" src={arrow} /></div></a>
+
+<div id = "down" class="overlay"></div>
+
+<footer class="footer"><p>Created by the Banana Bandits: Sean Choi, Chinmay Bansal, Jean-Pierre Ciotta, Sam Ahrens</p></footer>
+
+</body>
+
   </div>;
 }
